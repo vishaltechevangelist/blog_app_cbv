f6bcf239e37be7484e80dc2fca6dd469713544f6
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-03 21:19:07 +0530

    adding project, app level urls, views along with HttpResponse, NotFound, Redirect, use of reverse function

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+
+posts = [
+    {
+        "id": 1,
+        "title": "Understanding Django ORM",
+        "content": "Django ORM allows developers to interact with the database easily."
+    },
+    {
+        "id": 2,
+        "title": "Building REST APIs",
+        "content": "Django REST framework is powerful for building web APIs quickly."
+    },
+    {
+        "id": 3,
+        "title": "Handling Authentication",
+        "content": "Django provides built-in support for authentication and authorization."
+    }
+]
+
+
 def helloWorld(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("<html><h1>Hello World!</h1></html>")
+    html = ""
+    for post in posts:
+        html += "<html><div><h1>{} - {}</h1><p>{}</p></div></html>".format(post["id"],post["title"],post["content"])
+    return HttpResponse(html)
+
+def home(request):
+    # return HttpResponse("<html><h1>Hello World!</h1></html>")
+    html = ""
+    for post in posts:
+        html += "<html><div><a href='/post/{}'/><h1>{} - {}</h1></a><p>{}</p></div></html>".format(post["id"], post["id"],post["title"],post["content"])
+    return HttpResponse(html)
+
+def post(request, id):
+    valid_id = False
+    for post in posts:
+        if post['id'] == id:
+            html = "<html><div><h1>{} - {}</h1><p>{}</p></div></html>".format(post["id"],post["title"],post["content"])
+            valid_id = True
+            break
+    if valid_id:
+        return HttpResponse(html)
+    else:
+        return HttpResponseNotFound("Post not available😉")
+    
+def google(request, id):
+    # return HttpResponseRedirect("https://www.google.com")
+    # return HttpResponseRedirect("/post/{}/".format(id))
+    url = reverse("post", args=[id])
+    return HttpResponseRedirect(url)
+    
