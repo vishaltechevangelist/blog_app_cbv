37d4870e0dd63bcc1cd1e55d67dd6dd1cf0ba3bf
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-07 20:32:27 +0530

    updating posts models & applied migration

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,5 @@ from django.db import models
 class Post(models.Model):
     post_title = models.CharField(max_length=60)
     post_content = models.TextField()
-    published_date = models.DateTimeField(auto_now=True)
-
-
-
+    # published_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateField(auto_now=True) # updating data type
