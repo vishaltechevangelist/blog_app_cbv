5a11cf962a1dc35e29c3e97a0726bf9fd54768bc
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-12 23:20:16 +0530

    adding Model, ModelAdmin, Customizing Admin interface

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from posts.models import Post
 
 # Register your models here.
+
+
+# Decorator approach
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post_title', 'published_date'] # all are the fields same as of Post Model shown on admin interface
+    list_display_links = ['id', 'post_title'] # display as link on admin interface
+    list_filter = ['published_date']  # display filter on admin interface on field published date
+    search_fields = ['post_title'] # display search box, searches on post_title
+
+# admin.site.register(Post, PostAdmin) # Need to register PostAdmin (ModelAdmin) along with Post Model
